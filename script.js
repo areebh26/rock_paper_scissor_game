d5716e4ec3cb31734e32615534e364642be840f1
areeb <areebulhaq2005@gmail.com>
2025-11-05 14:41:21 +0500

    program completed

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let computerSore=0;
 let humanScore=0;
+playGame();
 function computerChoice(){
     let x=Math.floor(Math.random()*(3-1+1)+1);
     if(x===1){
@@ -17,36 +18,88 @@ function humanChoice(){
     answer = answer.toLowerCase();
     return answer;
 }
-function playRound(human , computer){
+function playRound(human=humanChoice() , computer=computerChoice()){
     if(human.charAt(0)===computer.charAt(0)){
+        console.log("You chose : " + human);
+        
+        console.log("Computer chose : " + computer);
+        
         console.log("Round tied !");
     }
     else if (human==="rock" && computer==="scissors"){
+        console.log("You chose : " + human);
+        console.log("Computer chose : " + computer);
         console.log("Rock beats Scissors. Round won by human");
         humanScore++;
     }
-    else if (human==="rock" && computer==="papaer"){
+    else if (human==="rock" && computer==="paper"){
+        console.log("You chose : " + human);
+        console.log("Computer chose : " + computer);
         console.log("Paper beats rock . Round won by computer");
         computerSore++;
     }
     else if (human==="paper" && computer==="scissors"){
+        console.log("You chose : " + human);
+        console.log("Computer chose : " + computer);
         console.log("Scissors beat paper . Round won by computer");
         computerSore++;
     }
     else if (human==="paper" && computer==="rock"){
+        console.log("You chose : " + human);
+        console.log("Computer chose : " + computer);
         console.log("Paper beats rock . Round won by human");
         humanScore++;
     }
     else if (human==="scissors" && computer==="paper"){
+        console.log("You chose : " + human);
+        console.log("Computer chose : " + computer);
         console.log("Scissors beat paper . Round won by human");
         humanScore++;
     }
     else if (human==="scissors" && computer==="rock"){
+        console.log("You chose : " + human);
+        console.log("Computer chose : " + computer);
         console.log("Rock beats Scissors . Round won by computer");
         computerSore++;
     }
 
 
+}
+function decideWinner(a,b){
+    if(a>b){
+        console.log("You won !");
+        
+    }
+    else if (a<b){
+        console.log("You Lost ! Computer won ");
+        
+    }
+    else{
+        console.log("Math tied");
+    }
+
+}
+function playGame(){
+    console.log("Lets start the game ! ");
+    playRound();
+    console.log("********");
+    console.log("********");
+    
+    
+    playRound();
+     console.log("********");
+    console.log("********");
+    playRound();
+     console.log("********");
+    console.log("********");
+    playRound();
+     console.log("********");
+    console.log("********");
+    playRound();
+     console.log("********");
+    console.log("********");
+    decideWinner(humanScore,computerSore);
+    
 }
 
 
